1a4cb8d7cfa158a06c4db2244e98457e0f27c4e6
Rick de Vries <rick.de.vries@hva.nl>
2017-12-08 13:22:55 +0100

    Mijn Bagage toegevoegd
    Rick’s gedeelte aangepast en toegevoegd

--- src/main/java/com/mybaggage/Database.java
+++ src/main/java/com/mybaggage/Database.java
@@ -5,10 +5,33 @@
  */
 package com.mybaggage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author rickdevries
  */
 public class Database {
-    
+
+    public static Connection Connect() {
+        try {
+            //De database connectie
+            String url = "jdbc:mysql://localhost:3306/bagage";
+            String user = "root";
+            String password = "root123";
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, user, password);
+            return conn;
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
 }
